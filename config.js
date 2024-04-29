7ee9eb16a5f054c4836d57089653c287e47a8ef6
Sambit Sargam Ekalabya <sambitsargam2003@gmail.com>
2024-04-29 20:46:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 
-export const wastemarketplaceAddress = "0x30B962f22A7C8D9f980fEc3670dbb44dc17B5BcB";
-export const arbwastemarketplaceAddress = "0x8628d91E8311B4856ae7d3E317a0E8fFf48E0f98";
+export const wastemarketplaceAddress = "0x5A8c2d7BDcC65a751C1037f961fe6A41e6055120";
+export const arbwastemarketplaceAddress = "0x5A8c2d7BDcC65a751C1037f961fe6A41e6055120";
